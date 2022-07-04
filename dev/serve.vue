1bf9398eefbbd9fdaed0b187ff02157d4bd21c76
Clyde Escobidal <cleidoscope@gmail.com>
2022-07-04 11:16:03 +0800

    Vue component fixes

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import VueSbchart from '@/vue-sbchart.vue';
+
+export default defineComponent({
+	name: 'ServeDev',
+	components: {
+		VueSbchart
+	}
+});
+</script>
+
+<template>
+	<div id="app">
+		<vue-sbchart />
+	</div>
+</template>
