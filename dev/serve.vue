be98f52f9ce4c8fdbedd994118f76a9fe751db0e
Clyde Escobidal <cleidoscope@gmail.com>
2023-08-31 11:45:31 +0800

    update README.md

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,26 @@
-<script>
-import { defineComponent } from 'vue';
+<script setup>
 import VueSbchart from '@/vue-sbchart.vue';
-
-export default defineComponent({
-	name: 'ServeDev',
-	components: {
-		VueSbchart
-	}
-});
+const series = [
+	{ timestamp: 1656818225, value: 12 },
+	{ timestamp: 1656818226, value: 13 },
+	{ timestamp: 1656818227, value: 14 },
+	{ timestamp: 1656818228, value: 15 },
+	{ timestamp: 1656818229, value: 12.5 },
+	{ timestamp: 1656818230, value: 13 },
+	{ timestamp: 1656818231, value: 12 }
+];
+const baseValue = 13;
+const options = {
+	upColor: '#37eb34',
+downColor: '#eb3434',
+lineWidth: 1
+};
 </script>
 
 <template>
 	<div id="app">
-		<vue-sbchart />
+		<div style="width: 500px; height: 230px">
+			<vue-sbchart :series="series" :baseValue="baseValue" :options="options" />
+		</div>
 	</div>
 </template>
