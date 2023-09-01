89bf23a67c5f2cacefdd51cae4f5a28798ce3cd4
clyde-tradearcade <cleidoscope@gmail.com>
2023-09-01 17:43:00 +0800

    build files

--- dist/vue-sbchart.js
+++ dist/vue-sbchart.js
@@ -1,8 +1,8 @@
-import { defineComponent as Y, computed as q, ref as T, onMounted as J, openBlock as b, createElementBlock as y, normalizeStyle as L, createCommentVNode as D, Fragment as K, renderList as Q, createElementVNode as S, toDisplayString as z, unref as F } from "vue";
-const X = ["src"], Z = {
+import { defineComponent as J, computed as j, ref as L, onMounted as K, openBlock as _, createElementBlock as $, normalizeStyle as M, createCommentVNode as z, Fragment as Q, renderList as X, normalizeClass as Z, createElementVNode as y, toDisplayString as F, unref as G, pushScopeId as P, popScopeId as H } from "vue";
+const x = (h) => (P("data-v-14e1e79c"), h = h(), H(), h), ee = ["src"], te = {
   key: 2,
   class: "simple-baseline-series"
-}, P = { class: "serie-value" }, x = /* @__PURE__ */ Y({
+}, le = { class: "simple-baseline-serie-value" }, re = /* @__PURE__ */ x(() => /* @__PURE__ */ y("span", { class: "arrow-down" }, null, -1)), oe = /* @__PURE__ */ J({
   __name: "VueSBChart",
   props: {
     series: {
@@ -34,100 +34,110 @@ const X = ["src"], Z = {
       default: !0
     }
   },
-  setup(w) {
-    const n = w, f = (e) => {
+  setup(h) {
+    const s = h, f = (e) => {
       var t = e.toString(16);
       return t.length == 1 ? "0" + t : t;
-    }, C = (e) => "#" + f(e.r) + f(e.g) + f(e.b), M = (e) => {
+    }, C = (e) => "#" + f(e.r) + f(e.g) + f(e.b), W = (e) => {
       const t = new Date(e * 1e3);
       return `${t.getDate()}/${t.getMonth()}/${t.getFullYear()} ${t.getHours()}:${t.getMinutes()}`;
-    }, k = q(() => n.series.sort((t, r) => t.timestamp - r.timestamp)), E = (e) => {
+    }, k = j(() => s.series.sort((t, l) => t.timestamp - l.timestamp)), O = (e) => {
       let t = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(e);
       return t ? { r: parseInt(t[1], 16), g: parseInt(t[2], 16), b: parseInt(t[3], 16) } : null;
-    }, s = E(n.options.upColor) || { r: 0, g: 128, b: 0 }, i = E(n.options.downColor) || { r: 255, g: 0, b: 0 }, G = n.options.lineWidth || 1, R = T(), O = T([]), m = T(0), I = T(), U = () => {
+    }, u = O(s.options.upColor) || { r: 0, g: 128, b: 0 }, p = O(s.options.downColor) || { r: 255, g: 0, b: 0 }, U = s.options.lineWidth || 1, B = L(), I = L([]), V = L(0), D = L(), S = j(() => {
+      var e;
+      return ((e = B.value) == null ? void 0 : e.clientWidth) / (s.series.length - 1);
+    }), A = () => {
       if (k.value.length == 0)
         return;
-      let e = document.createElement("canvas"), t = R.value;
-      e.width = t.offsetWidth * 2, e.height = t.offsetHeight * 2;
-      let r = e.getContext("2d");
-      r.lineWidth = 1;
-      let v = n.baseValue, o = k.value.map((W) => W.value), u = 1, p = o.length, g = u / p;
-      m.value = e.width / (p - 1) - g;
-      let h = e.height - u, c = Math.max.apply(Math, o), $ = Math.min.apply(Math, o), V = (v - $) / (c - $), d = (h - u) * V;
-      d < 0 && (d = 0);
-      let a = document.createElement("canvas");
-      a.width = e.width, a.height = e.height;
-      let l = a.getContext("2d");
-      N(r, k.value, c, $, h, u, m.value, d, e, "down"), l == null || l.drawImage(e, 0, 0), r == null || r.clearRect(0, 0, e.width, e.height), r == null || r.beginPath(), d > h ? e.height = 1 : e.height = h - d, e.height <= 0 && (e.height = 1), N(r, k.value, c, $, h, u, m.value, d, e, "up", !0), l == null || l.drawImage(e, 0, 0);
-      const _ = a.toDataURL("image/png");
-      I.value = _;
-    }, N = (e, t, r, v, o, u, p, g, h, c, $ = !1) => {
+      let e = document.createElement("canvas"), t = B.value;
+      e.width = t.clientWidth * 2, e.height = t.clientHeight * 2;
+      let l = e.getContext("2d");
+      l.lineWidth = 1;
+      let n = s.baseValue, a = k.value.map((w) => w.value), d = 0, m = a.length;
+      V.value = e.width / (m - 1);
+      let i = e.height - d, b = Math.max.apply(Math, a), c = Math.min.apply(Math, a), E = (n - c) / (b - c), g = (i - d) * E;
+      g < 0 && (g = 0);
+      let v = document.createElement("canvas");
+      v.width = e.width, v.height = e.height;
+      let o = v.getContext("2d");
+      R(l, k.value, b, c, i, d, V.value, g, e, "down"), o == null || o.drawImage(e, 0, 0), l == null || l.clearRect(0, 0, e.width, e.height), l == null || l.beginPath(), g > i ? e.height = 1 : e.height = i - g, e.height <= 0 && (e.height = 1), R(l, k.value, b, c, i, d, V.value, g, e, "up", !0), o == null || o.drawImage(e, 0, 0);
+      const r = v.toDataURL("image/png");
+      D.value = r;
+    }, R = (e, t, l, n, a, d, m, i, b, c, E = !1) => {
       e.beginPath();
-      let V = 0, d = `rgb(${s.r}, ${s.g}, ${s.b})`;
-      c == "down" && (d = `rgb(${i.r}, ${i.g}, ${i.b})`), t.forEach((l, _) => {
-        let W = (l.value - v) / (r - v), A = (o - u) * W, B = 0, j = o - A;
-        _ == 0 ? B = u : B = p * _, e.lineTo(B, j), _ > -1 && $ && n.interactive && O.value.push({
-          y: j / 2 - 5,
-          x: B / 2,
-          serie: l
+      let g = 0, v = `rgb(${u.r}, ${u.g}, ${u.b})`;
+      c == "down" && (v = `rgb(${p.r}, ${p.g}, ${p.b})`), t.forEach((r, w) => {
+        let Y = (r.value - n) / (l - n), q = (a - d) * Y, T = 0, N = a - q;
+        w == 0 ? T = d : T = m * w, e.lineTo(T, N), w > -1 && E && s.interactive && I.value.push({
+          y: N / 2 - 5,
+          x: T / 2,
+          serie: r
         });
-      }), e.strokeStyle = d, e.lineWidth = G, e.stroke();
-      let a;
+      }), e.strokeStyle = v, e.lineWidth = U, e.stroke();
+      let o;
       if (c == "up") {
-        a = e.createLinearGradient(0, 0, 0, o);
-        let l = (o - g) / o;
-        l > 1 ? l = 1 : l < 0 && (l = 0), a.addColorStop(0, `rgba(${s.r}, ${s.g}, ${s.b}, 0.5)`), a.addColorStop(l, `rgba(${s.r}, ${s.g}, ${s.b}, 0)`), e.lineTo(p * (t.length - 1), o - g), e.lineTo(p * (V - t.length), o - g);
+        o = e.createLinearGradient(0, 0, 0, a);
+        let r = (a - i) / a;
+        r > 1 ? r = 1 : r < 0 && (r = 0), o.addColorStop(0, `rgba(${u.r}, ${u.g}, ${u.b}, 0.5)`), o.addColorStop(r, `rgba(${u.r}, ${u.g}, ${u.b}, 0)`), e.lineTo(m * (t.length - 1), a - i), e.lineTo(m * (g - t.length), a - i);
       } else {
-        let l = g / o;
-        l > 1 ? l = 1 : l < 0 && (l = 0), a = e.createLinearGradient(0, o, 0, 0), a.addColorStop(0, `rgba(${i.r}, ${i.g}, ${i.b}, 0.5)`), a.addColorStop(l, `rgba(${i.r}, ${i.g}, ${i.b}, 0)`), e.lineTo(h.width - u, p), e.lineTo(u, p);
+        let r = i / a;
+        r > 1 ? r = 1 : r < 0 && (r = 0), o = e.createLinearGradient(0, a, 0, 0), o.addColorStop(0, `rgba(${p.r}, ${p.g}, ${p.b}, 0.5)`), o.addColorStop(r, `rgba(${p.r}, ${p.g}, ${p.b}, 0)`), e.lineTo(b.width - d, m), e.lineTo(d, m);
       }
-      c == "down" && (e.fillStyle = "transparent", e.fillRect(0, 0, h.width, o - g)), a && (e.fillStyle = a), e.globalCompositeOperation = "destination-over", e.fill(), e.globalCompositeOperation = "source-over";
+      c == "down" && (e.fillStyle = "transparent", e.fillRect(0, 0, b.width, a - i)), o && (e.fillStyle = o), e.globalCompositeOperation = "destination-over", e.fill(), e.globalCompositeOperation = "source-over";
     };
-    return J(() => {
-      U();
-    }), (e, t) => (b(), y("div", {
+    return K(() => {
+      A();
+    }), (e, t) => (_(), $("div", {
       ref_key: "simpleBaselineChart",
-      ref: R,
+      ref: B,
       class: "simple-baseline-chart"
     }, [
-      n.gridLines ? (b(), y("div", {
+      s.gridLines && B.value ? (_(), $("div", {
         key: 0,
         class: "simple-baseline-grid",
-        style: L({ backgroundSize: `${m.value / 2}px ${m.value / 2}px` })
-      }, null, 4)) : D("", !0),
-      I.value ? (b(), y("img", {
+        style: M({ backgroundSize: `${S.value}px ${S.value}px` })
+      }, null, 4)) : z("", !0),
+      D.value ? (_(), $("img", {
         key: 1,
-        src: I.value
-      }, null, 8, X)) : D("", !0),
-      n.interactive ? (b(), y("div", Z, [
-        (b(!0), y(K, null, Q(O.value, (r, v) => (b(), y("div", {
-          key: v,
-          class: "simple-baseline-serie",
-          style: L({ width: `${m.value / 2}px`, left: `${r.x}px` })
+        src: D.value
+      }, null, 8, ee)) : z("", !0),
+      s.interactive ? (_(), $("div", te, [
+        (_(!0), $(Q, null, X(I.value, (l, n) => (_(), $("div", {
+          key: n,
+          class: Z(["simple-baseline-serie", {
+            "simple-baseline-serie-start": n == 0,
+            "simple-baseline-serie-end": n == I.value.length - 1
+          }]),
+          style: M({
+            left: `${S.value * n - (n == 0 ? 0 : S.value / 2)}px`,
+            width: `${S.value / (n == 0 || n == I.value.length - 1 ? 2 : 1)}px`
+          })
         }, [
-          S("div", {
-            class: "serie-wrapper",
-            style: L({ top: `${r.y}px` })
+          y("div", {
+            class: "simple-baseline-serie-wrapper",
+            style: M({ top: `${l.y}px` })
           }, [
-            S("div", null, [
-              S("div", null, z(M(r.serie.timestamp)), 1),
-              S("div", P, z(r.serie.value), 1)
+            y("div", null, [
+              y("div", null, F(W(l.serie.timestamp)), 1),
+              y("div", le, F(l.serie.value), 1),
+              re
             ]),
-            S("span", {
-              style: L({ backgroundColor: `${r.serie.value > n.baseValue ? C(F(s)) : C(F(i))}` })
+            y("span", {
+              style: M({ backgroundColor: `${l.serie.value > s.baseValue ? C(G(u)) : C(G(p))}` })
             }, null, 4)
           ], 4)
-        ], 4))), 128))
-      ])) : D("", !0)
+        ], 6))), 128))
+      ])) : z("", !0)
     ], 512));
   }
 });
-const H = (w, n) => {
-  const f = w.__vccOpts || w;
-  for (const [C, M] of n)
-    f[C] = M;
+const ae = (h, s) => {
+  const f = h.__vccOpts || h;
+  for (const [C, W] of s)
+    f[C] = W;
   return f;
-}, te = /* @__PURE__ */ H(x, [["__scopeId", "data-v-6c9e2924"]]);
+}, ne = /* @__PURE__ */ ae(oe, [["__scopeId", "data-v-14e1e79c"]]);
 export {
-  te as VueSBChart
+  ne as VueSBChart
 };
